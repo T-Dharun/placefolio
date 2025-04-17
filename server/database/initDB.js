92768f81513bd2a0b99125a5c334dc57cd4860eb
T-Dharun <dharuneee007@gmail.com>
2025-04-17 09:31:39 +0530

    db changes

--- server/database/initDB.js
+++ server/database/initDB.js
@@ -51,15 +51,16 @@ const createTables = async () => {
 
         const createStudentCompanyTable = `
         CREATE TABLE IF NOT EXISTS Student_Company (
-            id INT AUTO_INCREMENT PRIMARY KEY,
             studentid VARCHAR(50),
             company_id INT,
             status VARCHAR(100),
             date VARCHAR(15),
+            PRIMARY KEY (studentid, company_id, status),
             FOREIGN KEY (studentid) REFERENCES students(roll_no) ON DELETE CASCADE,
             FOREIGN KEY (company_id) REFERENCES Company(company_id) ON DELETE CASCADE
-        );`;
-
+        );
+    `;
+    
         // Execute queries in proper order
         await db.query(createStudentsTable);
         console.log("Students table created successfully!");
